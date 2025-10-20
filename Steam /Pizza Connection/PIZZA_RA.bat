1cda220e67eea2c436aae1bcb27b280ef02acf84
FlameMan <evenent@gmail.com>
2025-10-20 17:43:40 +0200

    Update comments in PIZZA_RA.bat

--- Steam /Pizza Connection/PIZZA_RA.bat
+++ Steam /Pizza Connection/PIZZA_RA.bat
@@ -1,4 +1,4 @@
-#LAUNCH OPTIONS: "PIZZA_RA.bat" %command%
-#Author: FlameMan
+:: LAUNCH OPTIONS: "PIZZA_RA.bat" %command%
+:: Author: FlameMan
 
 ..\RetroArch\retroarch -L ..\RetroArch\cores\dosbox_pure_libretro.dll .\pt.exe
