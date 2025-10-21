957322f25e150ee57990813b7459fe7852f89fe3
FlameMan <evenent@gmail.com>
2025-10-21 07:01:58 +0200

    Create start_RA.bat for launching Rebel Assault
    Added a batch file to launch Rebel Assault games with RetroArch.

---
+++ Steam /Star Wars - Rebel Assault 1 and 2/start_RA.bat
@@ -0,0 +1,5 @@
+:: LAUNCH OPTIONS: "start_RA.bat" %command%
+:: Author: FlameMan
+
+..\..\RetroArch\retroarch -L ..\..\RetroArch\cores\dosbox_pure_libretro.dll ..\REBEL\REBEL.EXE
+:: ..\..\RetroArch\retroarch -L ..\..\RetroArch\cores\dosbox_pure_libretro.dll ..\REBEL2\REBEL2.EXE
