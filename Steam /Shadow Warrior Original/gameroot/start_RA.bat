6b6b5094b26793afcabeda4232fb675eb34607d4
FlameMan <evenent@gmail.com>
2025-10-23 08:56:25 +0200

    Add start_RA.bat to launch RetroArch for Shadow Warrior

---
+++ Steam /Shadow Warrior Original/gameroot/start_RA.bat
@@ -0,0 +1,5 @@
+:: LAUNCH OPTIONS: "start_RA.bat" %command%
+:: Author: FlameMan
+
+..\..\RetroArch\retroarch -L ..\..\RetroArch\cores\dosbox_pure_libretro.dll ..\gameroot\start.bat
+:: ..\..\RetroArch\retroarch -L ..\..\RetroArch\cores\dosbox_pure_libretro.dll ..\gameroot\betas.bat
