14124c84dd82d730c768e496b974a69464bd0750
FlameMan <evenent@gmail.com>
2025-10-23 13:46:41 +0200

    Create start_RA.bat

---
+++ Steam /Commander Keen/start_RA.bat
@@ -0,0 +1,8 @@
+:: LAUNCH OPTIONS: "start_RA.bat" %command%
+:: Author: FlameMan
+
+..\RetroArch\retroarch -L ..\RetroArch\cores\dosbox_pure_libretro.dll .\base1\KEEN1.EXE
+:: ..\RetroArch\retroarch -L ..\RetroArch\cores\dosbox_pure_libretro.dll .\base2\KEEN2.EXE
+:: ..\RetroArch\retroarch -L ..\RetroArch\cores\dosbox_pure_libretro.dll .\base3\KEEN3.EXE
+:: ..\RetroArch\retroarch -L ..\RetroArch\cores\dosbox_pure_libretro.dll .\base4\KEEN4E.EXE
+:: ..\RetroArch\retroarch -L ..\RetroArch\cores\dosbox_pure_libretro.dll .\base5\KEEN5E.EXE
