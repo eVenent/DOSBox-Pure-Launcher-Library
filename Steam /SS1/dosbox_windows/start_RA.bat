47e9220dd89c74329cbf2c2b292e95ce78bff722
FlameMan <evenent@gmail.com>
2025-11-04 14:36:08 +0100

    Create start_RA.bat

---
+++ Steam /SS1/dosbox_windows/start_RA.bat
@@ -0,0 +1,6 @@
+:: LAUNCH OPTIONS: "start_RA.bat" %command%
+:: Author: FlameMan
+
+:: ..\..\RetroArch\retroarch -L ..\..\RetroArch\cores\dosbox_pure_libretro.dll ..\start.bat
+..\..\RetroArch\retroarch -L ..\..\RetroArch\cores\dosbox_pure_libretro.dll ..\start_cd.bat
+:: ..\..\RetroArch\retroarch -L ..\..\RetroArch\cores\dosbox_pure_libretro.dll ..\start_fd.bat
